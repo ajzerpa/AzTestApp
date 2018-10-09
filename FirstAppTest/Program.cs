70045955df2a75747655450bfbeb07684775ac33
Armando José Zerpa Bustos <azerpa@estratec.local>
2018-10-09 12:01:24 -0500

    third commit. Version 3

--- FirstAppTest/Program.cs
+++ FirstAppTest/Program.cs
@@ -9,6 +9,9 @@ namespace FirstAppTest
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World AZ!!!");
+            var entry = Console.ReadLine();
+            if (!string.IsNullOrEmpty(entry))
+                entry = "Hello again";
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
